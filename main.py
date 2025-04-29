13111c4f925b4d4b3bcbdc6370200e1f5010f90a
archkb24 <adamkb2004@gmail.com>
2025-04-29 14:22:29 -0400

    Cleaned up some stuff

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import discord
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot
 
-utc = datetime.timezone.utc
 
 if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
     sys.exit("'config.json' not found! Please add it and try again.")
@@ -23,21 +22,22 @@ else:
 intents = discord.Intents.default()
 intents.message_content = True
 intents.polls = True
-# intents.members = True
 
+NUMBER_CHANNEL_ID = 1329591419983237201
 
 bot = Bot(
     command_prefix=commands.when_mentioned_or(config["prefix_settings"]["prefix"]),
     intents=intents
     )
-
 bot.config = config
+
 
 @bot.event
 async def on_ready():
     await bot.tree.sync()
     print('ALIVE!')
-    send_message.start() 
+    # start daily number loop
+    start_number.start()
 
 
 @bot.event
@@ -45,32 +45,34 @@ async def on_message(ctx):
     if ctx.author == bot.user:
         return
 
-    #logs
-    f = open(f"{os.path.realpath(os.path.dirname(__file__))}/david.log", "a")
-    timeNow = datetime.datetime.now()
-    f.write(f"{datetime.date.today()} {timeNow.hour}:{timeNow.minute}:{timeNow.second} ({ctx.guild.name} #{ctx.channel}) {ctx.author.name} said: '{ctx.content}'\n")
+    # logs (for debug)
+    # log_file = open(f"{os.path.realpath(os.path.dirname(__file__))}/david.log", "a")
+    # time_now = datetime.datetime.now()
+    # log_file.write(f"{datetime.date.today()} {time_now.hour}:{time_now.minute}:{time_now.second} ({ctx.guild.name} #{ctx.channel}) {ctx.author.name} said: '{ctx.content}'\n")
+    # log_file.close()
 
-    f.close()
     await bot.process_commands(ctx)
 
 
+# start number of the day everyday at noon EST
 number_hour = 12
 number_minute = 0
 est_tz = zoneinfo.ZoneInfo("America/New_York")
 loop_time = datetime.time(hour=number_hour, minute=number_minute, tzinfo=est_tz)
 @tasks.loop(time=loop_time)
-async def send_message():
-    NUMBER_ID = 1329591419983237201
-    channel = await bot.fetch_channel(NUMBER_ID)
+async def start_number():
+    # get channel for number
+    channel = await bot.fetch_channel(NUMBER_CHANNEL_ID) # future: change based on server
     await pickANumber.startRND(channel=channel)
 
 
+# reloads commands
 @bot.hybrid_command(name='reload')
 async def reload_cogs(ctx):
     state: discord.Message = await ctx.reply("Reloading Commands")
-    for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
-        if file.endswith(".py"):
-            extension = file[:-3]
+    for cog_file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
+        if cog_file.endswith(".py"):
+            extension = cog_file[:-3]
             try:
                 await bot.reload_extension(f"cogs.{extension}")
             except Exception as e:
@@ -78,10 +80,11 @@ async def reload_cogs(ctx):
     await state.edit(content="Reloaded Commands")
 
 
+# load slash commands
 async def load_cogs() -> None:
-    for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
-        if file.endswith(".py"):
-            extension = file[:-3]
+    for cog_file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
+        if cog_file.endswith(".py"):
+            extension = cog_file[:-3]
             try:
                 await bot.load_extension(f"cogs.{extension}")
             except Exception as e:
@@ -89,6 +92,8 @@ async def load_cogs() -> None:
 
 
 asyncio.run(load_cogs())
+# get token
 with open(f"{os.path.dirname(__file__)}/token.key") as f:
     TOKEN = f.read()
 bot.run(TOKEN)
+# future: get from sys var
